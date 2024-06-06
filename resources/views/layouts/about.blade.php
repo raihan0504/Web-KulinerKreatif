eee724d1fb57a5ca7b40f1e175514cefe1a155eb
CutDahliana <133567990+CutDahliana@users.noreply.github.com>
2024-06-06 10:51:07 +0700

    Update about.blade.php

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -6,13 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="About KulinerKreatif">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Menggunakan directive @vite untuk menyertakan file CSS -->
     @vite('resources/css/app.css')
     <title>About - KulinerKreatif</title>
 </head>
 
 <body class="bg-mainColor text-white tracking-wider min-h-screen flex flex-col bg-cover backdrop-blur-md" style="background-image: url('{{ asset('assets/bahan2.jpg') }}')">
+    <!-- Header -->
     <header class="bg-headerColor">
+        <!-- Navigasi -->
         <nav class="container flex justify-between items-center py-4 px-5">
+            <!-- Logo dan Form Pencarian -->
             <div class="w-20 text-orange font-bold text-3xl flex items-center gap-4">
                 <a href="/" class="flex items-center">
                     <span class="text-white">Kuliner</span>Kreatif
@@ -22,10 +26,12 @@
                     <input onkeydown="search(event)" id="search" placeholder="Cari" name="cari" type="text" class=" placeholder:text-[#b3b3b3] w-[360px] bg-white/0 rounded-2xl text-[16px] px-2 font-medium text-white focus:outline-none border border-white">
                 </form>
             </div>
+            <!-- Menu Navigasi -->
             <div>
                 <ul class="flex items-center space-x-6">
                     <li><a href="/" class="hover:text-blue ease-in duration-200">Home</a></li>
                     <li><a href="/about" class="hover:text-blue ease-in duration-200">About</a></li>
+                    <!-- Kondisi untuk menampilkan link Login atau Logout -->
                     @guest
                     <li><a href="/login" class="bg-blue px-9 py-3 rounded-md capitalize font-bold hover:opacity-80 ease-in duration-200">Login</a></li>
                     @else
@@ -40,7 +46,8 @@
             </div>
         </nav>
     </header>
-
+    
+    <!-- Konten Utama -->
     <main class="flex-grow">
         <div class="bg-white/10 rounded-lg shadow-md p-6 justify-center flex flex-col gap-5 box-border py-10 px-6 my-10 mx-10">
             <div class="px-10 gap-5">
@@ -54,9 +61,11 @@
             </div>
         </div>
     </main>
+
+    <!-- Menggunakan directive @vite untuk menyertakan file JavaScript -->
     @vite('resources/js/app.js')
 
-    {{-- footer --}}
+    <!-- Footer -->
     <footer class="bg-mainColor text-white">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
             <span class="block text-center w-full">
